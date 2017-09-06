4e82df747a17adb6517cdfda7244361c39ff880e
WeiTsungCheng <sanctuary85c@gmail.com>
2017-09-06 17:18:06 +0800

    AVPlayer1

---
+++ VideoPlayer/VideoPlayer/VideoViewController.swift
@@ -0,0 +1,134 @@
+//
+//  VideoViewController.swift
+//  VideoPlayer
+//
+//  Created by Wei-Tsung Cheng on 2017/9/6.
+//  Copyright © 2017年 Wei-Tsung Cheng. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+import AVKit
+
+class VideoViewController: UIViewController {
+
+
+
+  //  var tableView: UITableView!
+    var searchController: UISearchController!
+
+    var playButton: UIButton!
+    var pauseButton: UIButton!
+    var videoView: UIView!
+    var player:AVPlayer!
+    var Bool:Bool! = false
+
+    func clickPlayButton() {
+
+
+        if Bool == true{
+            player.pause()
+            Bool = false
+        }else{
+            player.play()
+            Bool = true
+        }
+
+
+    }
+
+    func clickMuteButton() {
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        videoView = UIView(frame: CGRect(x: 0, y: 64, width: 375, height: 564))
+        videoView.backgroundColor = .white
+        self.view.addSubview(videoView)
+
+        playButton = UIButton(frame: CGRect(x: 0, y: 623, width: 74, height: 44))
+        playButton.setTitleColor(.white, for: .normal)
+
+        playButton.setTitle("Paly", for: .normal)
+        playButton.isEnabled = true
+
+        playButton.addTarget(
+            self,
+            action: #selector(VideoViewController.clickPlayButton),
+            for: .touchUpInside)
+
+        pauseButton = UIButton(frame: CGRect(x: 304, y: 623, width: 74, height: 44))
+        pauseButton.setTitle("Mute", for: .normal)
+        pauseButton.isEnabled = true
+
+        pauseButton.addTarget(
+            self,
+            action: #selector(VideoViewController.clickMuteButton),
+            for: .touchUpInside)
+
+
+        self.view.addSubview(playButton)
+        self.view.addSubview(pauseButton)
+
+        self.view.backgroundColor = UIColor.black
+
+        print("good")
+
+
+       self.searchController =
+            UISearchController(searchResultsController: nil)
+
+        self.searchController.searchBar.searchBarStyle =
+            .prominent
+
+        searchController.searchBar.barTintColor = UIColor.black
+
+        searchController.searchBar.frame = CGRect(x: 8, y: 20, width: 359, height: 30)
+
+        searchController.searchBar.placeholder = "Enter URL of video"
+
+        self.view.addSubview(searchController.searchBar)
+
+
+
+
+
+
+
+        let url = URL(string: "http://devimages.apple.com/iphone/samples/bipbop/bipbopall.m3u8")
+
+        player = AVPlayer(url: url!)
+        let playerLayer = AVPlayerLayer(player: player)
+        playerLayer.frame = self.videoView.bounds
+        self.videoView.layer.addSublayer(playerLayer)
+        player.play()
+        Bool = true
+
+
+        // Do any additional setup after loading the view.
+    }
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
